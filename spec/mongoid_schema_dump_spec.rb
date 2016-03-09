d166da51a2a42c9e95caac4ef50c1c18c39ff0b1
rpechayr <romain.pechayre@applidget.com>
2016-03-09 10:05:52 +0100

    Add test

--- spec/mongoid_schema_dump_spec.rb
+++ spec/mongoid_schema_dump_spec.rb
@@ -24,13 +24,21 @@ class Commit
   include Mongoid::Timestamps
   
   belongs_to :repository
+  has_many :comments, as: :commentable
   field :commit_id
+end
+
+class Comment 
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  belongs_to :commentable, polymorphic: true
 end
 
 describe MongoidSchemaDump do
   
   it 'returns all Mongoid models' do
-    expect(MongoidSchemaDump.models.sort).to eq(["Organization", "Repository", "Commit"].sort)
+    expect(MongoidSchemaDump.models.sort).to eq(["Organization", "Repository", "Commit", "Comment"].sort)
   end
   
   it "returns all dependencies" do
@@ -42,6 +50,7 @@ describe MongoidSchemaDump do
     expect(mapping["organizations"]).to be(nil)
     expect(mapping["repositories"]["dependency"]).to eq("organizations")
     expect(mapping["commits"]["dependency"]).to eq("repositories")
+    expect(mapping["comments"]["dependency"]).to eq("commits")
   end
 
 end
